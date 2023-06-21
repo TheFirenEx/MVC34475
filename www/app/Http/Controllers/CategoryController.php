358c2a6343fcfc0491fce912408066c24dcea68c
Piotr Knychała <p.knychala@pwsz-kalisz.edu.pl>
2023-06-21 13:27:39 +0200

    FEAT10 Wyświetlanie kategorii

--- www/app/Http/Controllers/CategoryController.php
+++ www/app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace Http\Controllers;
 
+use Views\View;
 use Database\PdoHandle;
 use Helpers\AbsoluteUrl;
 use Http\Controllers\Controller;
@@ -10,18 +11,24 @@ use Models\Repositories\CategoriesRepository;
 class CategoryController extends Controller
 {
     private $categoryRepository;
+    private $view;
 
     public function __construct()
     {
         $this->categoryRepository = new CategoriesRepository(
             new PdoHandle()
         );
+        $this->view = new View();
     }
 
     public function index()
     {
-        d(
-            $this->categoryRepository->all()
+        $this->view->render(
+            'categories',
+            [
+                'title' => 'Kategorie',
+                'categories' => $this->categoryRepository->all(),
+            ]
         );
     }
 }
