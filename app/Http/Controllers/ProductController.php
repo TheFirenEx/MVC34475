40c78bb6950f1ad659fa2f16ede169ccf027e5d2
Firen <the.firen.ex@gmail.com>
2025-10-21 15:55:37 +0000

    modyfikacja ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,32 @@
 
 namespace Http\Controllers;
 
-class ProductController
+use Views\View;
+use Database\PdoHandle;
+use Models\Repositories\ProductsRepository;
+
+class ProductController extends Controller
 {
+    private $productRepository;
+    private $view;
+
+    public function __construct()
+    {
+        $this->productRepository = new ProductsRepository(
+            new PdoHandle()
+        );
+        $this->view = new View();
+    }
+
     public function index()
     {
-        d('ProductController');
+        $this->view->render(
+            'products',
+            [
+                'title' => 'Produkty',
+                'products' => $this->productRepository->all(),
+            ]
+        );
     }
 
 }
--- app/Models/Repositories/ProductsRepository.php
+++ app/Models/Repositories/ProductsRepository.php
@@ -2,7 +2,44 @@
 
 namespace Models\Repositories;
 
-class ProductsRepository
-{
+use PDO;
+use Models\Model;
+use PDOException;
+use Models\Product;
+use Exceptions\DatabaseException;
+use Models\Repositories\Repository;
 
+class ProductsRepository extends Repository
+{
+    protected $table = 'products';
+
+    public function create(array $arguments): Product
+    {
+        return new Product($arguments);
+    }
+
+    public function insert(Model $model): int
+    {
+        if (!$model instanceof Product) {
+            return -1;
+        }
+        try {
+            $query = 'INSERT INTO `' . $this->table . '`';
+            $query .= '(`name`)';
+            $query .= ' VALUES (:name)';
+            $stmt = $this->handle->getHandle()->prepare($query);
+            $stmt->bindValue(
+                ':name',
+                $model->name,
+                PDO::PARAM_STR
+            );
+            if ($stmt->execute()) {
+                return $this->handle->getHandle()
+                    ->lastInsertId();
+            }
+            return -1;
+        } catch (PDOException $e) {
+            throw new DatabaseException($e);
+        }
+    }
 }
